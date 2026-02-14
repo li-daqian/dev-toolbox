fe8bd28512470271fce503505ae4e210a87d6226
Li Daqian <hi@lidaqian.me>
2026-02-15 06:52:30 +0800

    feat: add Docker installation steps and configure GPG key

--- install.sh
+++ install.sh
@@ -85,6 +85,26 @@ if ! command_exists nvm; then
     zsh -c "source ~/.zshrc && nvm install --lts && npm install -g pnpm"
 fi
 
+# Install Docker
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt -y install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 # Install Rime
 curl -fsSL "https://raw.githubusercontent.com/li-daqian/dev-toolbox/main/rime/install.sh?$(date +%s)" | sh
 
