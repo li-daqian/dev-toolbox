32c98cf1000a5ced0dff89e54fd6ed667e6cf089
Li Daqian <hi@lidaqian.me>
2026-02-17 08:34:50 +0800

    feat: add autostart configuration for Albert on login

--- install.sh
+++ install.sh
@@ -164,6 +164,18 @@ if ! command_exists albert; then
     curl -fsSL https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_24.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_manuelschneid3r.gpg > /dev/null
     sudo apt update
     sudo apt install -y albert
+    # Make Albert start on login
+    mkdir -p ~/.config/autostart
+    cat > ~/.config/autostart/albert.desktop <<EOL
+[Desktop Entry]
+Type=Application
+Exec=albert
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=true
+Name=Albert
+Comment=Start Albert on login
+EOL
     # Add Custom Shortcuts ‘Alt+Space‘ for Albert
     add_custom_shortcut "albert-show" "Show Albert" "albert toggle" "<Alt>space"
 else
