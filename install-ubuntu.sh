b7a496fcf3b522cf250e70671f2e182fc4376892
Li Daqian <hi@lidaqian.me>
2026-02-26 14:40:51 +0800

    feat: add setting to show weekday in top bar

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -84,6 +84,8 @@ gsettings set org.gnome.desktop.interface text-scaling-factor 1.2
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 # Make Dock icons centered
 gsettings set org.gnome.shell.extensions.dash-to-dock always-center-icons true
+# Make top bar show weekday
+gsettings set org.gnome.desktop.interface clock-show-weekday true
 # Make Dock max-icon-size to 38
 gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 38
 # Enable System Monitor extension, only show CPU/Memory/Download in top bar
